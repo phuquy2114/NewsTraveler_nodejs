123ac9b13a100ab6ca5a030b576d5c83e8c83272
uits <vnfdnservice@gmail.com>
2020-10-21 20:49:41 +0700

    change

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ createConnection({
     "host": "localhost",
     "port": 3306,
     "username": "root",
-    "password": "root1234",
+    "password": "",
     "database": "news_traveller",
     "synchronize": true,
     "logging": false,
